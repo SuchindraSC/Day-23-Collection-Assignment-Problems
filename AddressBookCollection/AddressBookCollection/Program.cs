9e2d84539feed90f1851e2421361ae91dcc9a4c5
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-28 17:21:38 +0530

    Day 23 Collection Assignment Problems

---
+++ AddressBookCollection/AddressBookCollection/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AddressBookCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("##################Welcome to the Address Book Program################");
+            AddressBookBuilder abd = new AddressBookBuilder();
+            while (true)
+            {
+                Console.WriteLine("*********************************************************");
+                Console.WriteLine("1. Add Member to Contact list \n2. View Members in Contact List\n3. Edit members Contacts lists\n4. Delete members Contacts list\n5. Search Details\n6. Count\n7. SortEntries\n8. Exit");
+                Console.WriteLine("Enter an option:");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        abd.AddContact();
+                        break;
+                    case 2:
+                        abd.ViewContactPerson();
+                        break;
+                    case 3:
+                        abd.editingDetails();
+                        break;
+                    case 4:
+                        abd.DeleteContact();
+                        break;
+                    case 5:
+                        abd.SearchDetails();
+                        break;
+                    case 6:
+                        abd.CountByStateOrCity();
+                        break;
+                    case 7:
+                        abd.SortEntries();
+                        break;
+                    case 8:
+                        Console.WriteLine("Exited");
+                        return;
+                }
+
+            }
+
+        }
+    }
+}
